9ba905d6cc118202c942f2b0b4999a4e4b511dc3
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-02-07 17:53:17 +0000

    flipper and ball skeleton

---
+++ src/main/java/strath/cs308/gizmo/dycoll/model/Ball.java
@@ -0,0 +1,5 @@
+package strath.cs308.gizmo.dycoll.model;
+
+public class Ball extends PhysicalBody
+{
+}
---
+++ src/main/java/strath/cs308/gizmo/dycoll/model/Flipper.java
@@ -0,0 +1,5 @@
+package strath.cs308.gizmo.dycoll.model;
+
+public class Flipper extends PhysicalBody
+{
+}
