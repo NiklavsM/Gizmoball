5eac115e74619e83042f004c2bb36494176fff5d
Ioan Luca <ioan.luca.2016@uni.strath.ac.uk>
2018-02-08 17:46:21 +0000

    view files for debugview

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import gui.DebugView;
 import javafx.application.Application;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 import model.BencesBall;
+import model.GameModel;
+import model.IGameModel;
 import model.Wall;
+import physics.Vect;
 
 public class Main extends Application {
 
@@ -18,21 +23,19 @@ public class Main extends Application {
         Wall wall4 = new Wall(20, 20, 0, 20);
 
         BencesBall ball = new BencesBall(10, 10, 1);
-//        ball.setVelocity(new Vect(1, 1));
+        ball.setVelocity(new Vect(1, 20));
 
-//        IGameModel world = new GameModel();
-//        world.addGizmo(ball);
-//        world.addGizmo(wall1);
-//        world.addGizmo(wall2);
-//        world.addGizmo(wall3);
-//        world.addGizmo(wall4);
-
-
-//        DebugView view = new DebugView(primaryStage, world);
+        IGameModel world = new GameModel();
+        world.addGizmo(ball);
+        world.addGizmo(wall1);
+        world.addGizmo(wall2);
+        world.addGizmo(wall3);
+        world.addGizmo(wall4);
 
 
-//        primaryStage.setFullScreen(true);
-//        primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        DebugView view = new DebugView(primaryStage, world);
+
+
         primaryStage.show();
     }
 }
