a64be1ee3aa6b87cc0a7c70161c4aadeac17bbf4
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-02-08 04:19:19 +0000

    ball basic impl

--- src/main/java/strath/cs308/gizmo/dycoll/model/Ball.java
+++ src/main/java/strath/cs308/gizmo/dycoll/model/Ball.java
@@ -1,5 +1,21 @@
 package strath.cs308.gizmo.dycoll.model;
 
+import mit.physics.Circle;
+import mit.physics.Vect;
+
 public class Ball extends PhysicalBody
 {
+    public Ball(double positionX, double positionY, double radius)
+    {
+        super();
+        
+        Vect midlePoint = new Vect(positionX, positionY);
+        Circle circle = new Circle(midlePoint, radius);
+    }
+
+    @Override
+    public void move(long time)
+    {
+
+    }
 }
