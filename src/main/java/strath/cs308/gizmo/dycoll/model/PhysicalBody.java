22d9a46a6773f7583fa8825b42f5c2985ac099ee
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-02-08 03:48:14 +0000

    physicsal body physics functions

--- src/main/java/strath/cs308/gizmo/dycoll/model/PhysicalBody.java
+++ src/main/java/strath/cs308/gizmo/dycoll/model/PhysicalBody.java
@@ -1,5 +1,45 @@
 package strath.cs308.gizmo.dycoll.model;
 
-public class PhysicalBody implements IPhysicalBody
+import mit.physics.Angle;
+import mit.physics.Circle;
+import mit.physics.LineSegment;
+import mit.physics.Vect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PhysicalBody implements IPhysicalBody
 {
+    protected List<Circle> circles;
+    protected List<LineSegment> lineSegments;
+    protected Vect velocity;
+
+    public PhysicalBody()
+    {
+        this.circles = new ArrayList<>();
+        this.lineSegments = new ArrayList<>();
+        this.velocity = new Vect(Angle.ZERO, 0.0);
+    }
+
+    public List<Circle> getCircles()
+    {
+        return circles;
+    }
+
+    public List<LineSegment> getLineSegments()
+    {
+        return lineSegments;
+    }
+
+    public Vect getVelocity()
+    {
+        return velocity;
+    }
+
+    public void setVelocity(Vect velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    abstract public void move(long time);
 }
