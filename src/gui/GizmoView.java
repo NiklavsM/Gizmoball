90c2cfa2b7b1e205777e2e5b7cb4b8e03f489324
Niklavs Meiers <niklavs@burti.lv>
2018-02-12 20:56:25 +0000

    Fixes the disappearing gizmos (had the same ids).

--- src/gui/GizmoView.java
+++ src/gui/GizmoView.java
@@ -4,11 +4,8 @@ package gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import model.IGameModel;
-import model.gizmo.Absorber;
-import model.gizmo.CircleGizmo;
+import model.gizmo.*;
 import model.GameModel;
-import model.gizmo.Square;
-import model.gizmo.Triangle;
 
 public class GizmoView extends Application {
 
@@ -50,28 +47,31 @@ public class GizmoView extends Application {
         gameModel.setBallSpeed(8, 8);
 
         gameModel.addGizmo(new Square(0, 5, "1"));
-        gameModel.addGizmo(new CircleGizmo(1, 5, "2"));
-        gameModel.addGizmo(new CircleGizmo(2, 5, "3"));
-        gameModel.addGizmo(new CircleGizmo(3, 5, "4"));
-        gameModel.addGizmo(new CircleGizmo(4, 5, "5"));
-        gameModel.addGizmo(new CircleGizmo(5, 6, "5"));
-        gameModel.addGizmo(new CircleGizmo(5, 7, "6"));
-        gameModel.addGizmo(new CircleGizmo(6, 8, "7"));
-        gameModel.addGizmo(new Square(15, 8, "8"));
-        gameModel.addGizmo(new CircleGizmo(14, 8, "9"));
-        gameModel.addGizmo(new CircleGizmo(13, 8, "0"));
-        gameModel.addGizmo(new CircleGizmo(17, 6, "id1"));
+        gameModel.addGizmo(new CircleGizmo(1, 5));
+        gameModel.addGizmo(new CircleGizmo(2, 5));
+        gameModel.addGizmo(new CircleGizmo(3, 5));
+        gameModel.addGizmo(new CircleGizmo(4, 5));
+        gameModel.addGizmo(new CircleGizmo(5, 6));
+        gameModel.addGizmo(new CircleGizmo(5, 7));
+        gameModel.addGizmo(new CircleGizmo(6, 8));
+        gameModel.addGizmo(new Square(15, 8));
+        gameModel.addGizmo(new CircleGizmo(14, 8));
+        gameModel.addGizmo(new CircleGizmo(13, 8));
+        gameModel.addGizmo(new CircleGizmo(17, 6));
 
-        gameModel.addGizmo(new CircleGizmo(10, 8, "x1"));
-        gameModel.addGizmo(new CircleGizmo(11, 9, "x2"));
-        gameModel.addGizmo(new CircleGizmo(12, 10, "x3"));
-        gameModel.addGizmo(new Square(3, 14, "s"));
-        gameModel.addGizmo(new Square(4, 14, "ss"));
-        gameModel.addGizmo(new Square(5, 14, "sss"));
+        gameModel.addGizmo(new CircleGizmo(10, 8));
+        gameModel.addGizmo(new CircleGizmo(11, 9));
+        gameModel.addGizmo(new CircleGizmo(12, 10));
+        gameModel.addGizmo(new Square(3, 14));
+        gameModel.addGizmo(new Square(4, 14));
+        gameModel.addGizmo(new Square(5, 14));
 
-        gameModel.addGizmo(new CircleGizmo(7, 7, "id1"));
-        gameModel.addGizmo(new Triangle(19, 0, "id1"));
-        gameModel.addGizmo(new Absorber(0, 19, 20, 20, "id1"));
+        gameModel.addGizmo(new CircleGizmo(7, 7));
+        IGizmo triange = new Triangle(19, 0);
+        triange.rotate();
+        triange.rotate();
+        gameModel.addGizmo(triange);
+        gameModel.addGizmo(new Absorber(0, 19, 20, 20));
 
         return gameModel;
     }
