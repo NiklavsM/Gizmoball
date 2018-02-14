fab824d248db637d32662c2633e2887b4259ac63
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-02-14 19:35:50 +0000

    flipper 90 movement

--- src/model/Flipper.java
+++ src/model/Flipper.java
@@ -4,6 +4,10 @@ import mit.physics.*;
 
 public class Flipper extends Gizmo implements IMovable {
 
+    enum Movement {
+        BACK, FORWARD, STILL;
+    }
+
     private Circle startPoint;
     private Circle endPoint;
     private LineSegment connector1;
@@ -11,19 +15,19 @@ public class Flipper extends Gizmo implements IMovable {
 
     private Vect velocity;
     private boolean isMoving;
-
-    //FORTESTING
-    private double moveCooldown;
+    private double movedAngle;
+    private Movement movementStatus;
 
     public Flipper(String name, double startX, double startY) {
         super(name);
 
         double radius = 0.25;
 
-        isMoving = false;
-        moveCooldown = 2;
+        movementStatus = Movement.FORWARD;
+        movedAngle = Angle.ZERO.radians();
 
-        velocity = new Vect(new Angle(-1.57079633));
+        velocity = new Vect(Angle.DEG_90);
+
 
         startPoint = new Circle(startX + radius, startY + radius, radius);
         endPoint = new Circle(startX + radius, startY + radius + 1.5, radius);
@@ -57,26 +61,53 @@ public class Flipper extends Gizmo implements IMovable {
 
     @Override
     public void move(double time) {
-        Circle rotated = Geometry.rotateAround(endPoint
-                , startPoint.getCenter()
-                , new Angle(velocity.angle().radians() * time));
+        double rotationRadian = velocity.angle().radians() * time;
+        if (movementStatus == Movement.FORWARD) {
+            rotationRadian *= -1;
+        }
+        Angle rotationAngle = new Angle(rotationRadian);
+        movedAngle += Math.abs(rotationRadian) ;
+
+        if (movedAngle > Angle.DEG_90.radians()) {
+
+            switch (movementStatus) {
+                case FORWARD:
+                    movementStatus = Movement.BACK;
+                break;
+
+                case BACK:
+                    movementStatus = Movement.STILL;
+                break;
+            }
+
+            movedAngle = Angle.ZERO.radians();
+        }
+
+        if (movementStatus != Movement.STILL) {
 
 
-        circles.remove(endPoint);
-        endPoint = rotated;
-        circles.add(endPoint);
-        lines.remove(connector1);
-        lines.remove(connector2);
 
-        connector1 = Geometry.rotateAround(connector1
-                , startPoint.getCenter()
-                , new Angle(velocity.angle().radians() * time));
-        connector2 = Geometry.rotateAround(connector2
-                , startPoint.getCenter()
-                , new Angle(-1.57079633 * time));
+            Circle rotated = Geometry.rotateAround(endPoint
+                    , startPoint.getCenter()
+                    , rotationAngle);
 
-        lines.add(connector1);
-        lines.add(connector2);
+
+            circles.remove(endPoint);
+            endPoint = rotated;
+            circles.add(endPoint);
+            lines.remove(connector1);
+            lines.remove(connector2);
+
+            connector1 = Geometry.rotateAround(connector1
+                    , startPoint.getCenter()
+                    , rotationAngle);
+            connector2 = Geometry.rotateAround(connector2
+                    , startPoint.getCenter()
+                    , rotationAngle);
+
+            lines.add(connector1);
+            lines.add(connector2);
+        }
     }
 
     @Override
