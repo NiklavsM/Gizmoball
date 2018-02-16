1d5f85320a8538d7a6a211916e447b06be1ea4c9
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-02-16 01:26:58 +0000

    drawing gizmos on editor mode

--- src/strath/cs308/gizmoball/view/EditorView.java
+++ src/strath/cs308/gizmoball/view/EditorView.java
@@ -3,6 +3,8 @@ package strath.cs308.gizmoball.view;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -10,17 +12,21 @@ import strath.cs308.gizmoball.controller.TopToolbarEventHandler;
 import strath.cs308.gizmoball.model.IGameModel;
 
 import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
 
-public class EditorView implements IEditorView
+public class EditorView implements IEditorView, Observer
 {
     private BorderPane root;
     private IGameModel gameModel;
+    private Canvas canvas;
 
     public EditorView(Stage stage, IGameModel gameModel) {
 
         try
         {
             root = FXMLLoader.load(getClass().getResource("/view/editorview.fxml"));
+            canvas = (Canvas) root.lookup("#canvas");
             this.gameModel = gameModel;
 
             Scene scene = new Scene(root);
@@ -29,7 +35,11 @@ public class EditorView implements IEditorView
             stage.setTitle("Gizmoball - Editor");
             stage.show();
 
-            Platform.runLater(this::attachHandlers);
+            Platform.runLater(() -> {
+                drawBackground();
+                drawGizmos();
+                attachHandlers();
+            });
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,5 +59,22 @@ public class EditorView implements IEditorView
     @Override
     public void switchToPlay() {
         PlayView playView = new PlayView((Stage) root.getScene().getWindow(), gameModel);
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        drawBackground();
+        drawGizmos();
+    }
+
+    private void drawGizmos() {
+        GizmoDrawer gizmoDrawer = new GizmoDrawer(canvas);
+        gameModel.getGizmos().forEach(gizmoDrawer::drawGizmo);
+    }
+
+    private void drawBackground() {
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.setFill(GizmoDrawer.DEEP_BLUE);
+        graphicsContext.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 }
