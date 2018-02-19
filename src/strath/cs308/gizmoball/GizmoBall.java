ecee6563e4fd75f5001dcbdd6080cc91b4c48dec
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-02-19 16:42:43 +0000

    asbosrber shooting prototype

--- src/strath/cs308/gizmoball/GizmoBall.java
+++ src/strath/cs308/gizmoball/GizmoBall.java
@@ -3,20 +3,30 @@ package strath.cs308.gizmoball;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import strath.cs308.gizmoball.model.GameModel;
+import strath.cs308.gizmoball.model.GameTimer;
 import strath.cs308.gizmoball.model.IGameModel;
 import strath.cs308.gizmoball.model.gizmo.*;
 import strath.cs308.gizmoball.view.PlayView;
 
 public class GizmoBall extends Application {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        Absorber absorber = new Absorber(0, 19, 20, 20);
+        Ball ball = new Ball(5, 5);
+
         IGameModel gameModel = new GameModel();
+        gameModel.addGizmo(absorber);
+        gameModel.addGizmo(ball);
+
+        GameTimer gameTimer =  new GameTimer(gameModel);
+        gameTimer.start();
+
         PlayView playView = new PlayView(stage, gameModel);
 
     }
