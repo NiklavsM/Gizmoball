39e2dcd27f6ed701b07370887be6d8e23125789b
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-02-20 06:28:12 +0000

    missing move gizmo controller

---
+++ src/strath/cs308/gizmoball/controller/strategy/MoveGizmoStrategy.java
@@ -0,0 +1,28 @@
+package strath.cs308.gizmoball.controller.strategy;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import strath.cs308.gizmoball.model.IGameModel;
+import strath.cs308.gizmoball.model.gizmo.IGizmo;
+import strath.cs308.gizmoball.view.IEditorView;
+
+public class MoveGizmoStrategy implements EventHandler<MouseEvent>
+{
+    private final IEditorView editorView;
+    private final IGameModel gameModel;
+
+    public MoveGizmoStrategy(IGameModel gameModel, IEditorView editorView) {
+        this.gameModel = gameModel;
+        this.editorView = editorView;
+    }
+
+    @Override
+    public void handle(MouseEvent mouseEvent) {
+
+        double pointX = Math.floor(mouseEvent.getX() / editorView.getPixelRatioFor(20.0));
+        double pointY = Math.floor(mouseEvent.getY() / editorView.getPixelRatioFor(20.0));
+
+        IGizmo gizmo = gameModel.getGizmo(pointX, pointY);
+        gameModel.remove(gizmo.getId());
+    }
+}
