3dc19ecc877826d82dc003c04ad030b266f7135e
Lyubomir Ivanov <kwb15150@uni.strath.ac.uk>
2018-02-24 04:24:43 +0000

    fix adding multiple gizmos on top of eachother

--- src/strath/cs308/gizmoball/controller/strategy/AddGizmoStrategy.java
+++ src/strath/cs308/gizmoball/controller/strategy/AddGizmoStrategy.java
@@ -27,17 +27,18 @@ public class AddGizmoStrategy implements EventHandler<MouseEvent> {
 
         double pointX = mouseEvent.getX() / editorView.getPixelRatioFor(20.0);
         double pointY = mouseEvent.getY() / editorView.getPixelRatioFor(20.0);
-        if (!gizmoType.equals(IGizmo.Type.BALL)) {
-            pointX = Math.floor(pointX);
-            pointY = Math.floor(pointY);
+
+        pointX = Math.floor(pointX);
+        pointY = Math.floor(pointY);
+
+        if (gameModel.getGizmo(pointX, pointY) == null ) {
+            if (!gizmoType.equals(IGizmo.Type.ABSORBER)) {
+                IGizmo gizmo = gizmoFactory.createGizmo(gizmoType, pointX, pointY);
+                System.out.println(gizmo);
+                gameModel.addGizmo(gizmo);
+            }
+        } else {
+            System.out.println("A " + gameModel.getGizmo(pointX, pointY).getType().toString() + " is already occupying this square. Gizmo cannot be placed here!");
         }
-
-        if (!gizmoType.equals(IGizmo.Type.ABSORBER)) {
-
-            IGizmo gizmo = gizmoFactory.createGizmo(gizmoType, pointX, pointY);
-            System.out.println(gizmo);
-            gameModel.addGizmo(gizmo);
-        }
-
     }
 }
