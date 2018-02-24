f9fbf8c00588233915650cd08275064c96a79627
Lyubomir Ivanov <kwb15150@uni.strath.ac.uk>
2018-02-24 06:09:04 +0000

    fixed multiple flippers concentrated in a 2x2 grid but still some undesired behaviour

--- src/strath/cs308/gizmoball/controller/strategy/AddGizmoStrategy.java
+++ src/strath/cs308/gizmoball/controller/strategy/AddGizmoStrategy.java
@@ -1,7 +1,10 @@
 package strath.cs308.gizmoball.controller.strategy;
 
 import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 import strath.cs308.gizmoball.model.GizmoFactory;
 import strath.cs308.gizmoball.model.IGameModel;
 import strath.cs308.gizmoball.model.IGizmoFactory;
@@ -31,14 +34,45 @@ public class AddGizmoStrategy implements EventHandler<MouseEvent> {
         pointX = Math.floor(pointX);
         pointY = Math.floor(pointY);
 
-        if (gameModel.getGizmo(pointX, pointY) == null ) {
-            if (!gizmoType.equals(IGizmo.Type.ABSORBER)) {
-                IGizmo gizmo = gizmoFactory.createGizmo(gizmoType, pointX, pointY);
-                System.out.println(gizmo);
-                gameModel.addGizmo(gizmo);
+        try {
+            if (gameModel.getGizmo(pointX, pointY) == null) {
+                if (!gizmoType.equals(IGizmo.Type.ABSORBER)) {
+
+                    if (gizmoType.equals(IGizmo.Type.LEFT_FLIPPER) || gizmoType.equals(IGizmo.Type.RIGHT_FLIPPER)) {
+
+                        //adjust which squares to scan if free depending on the flipper orientation
+                        if (gizmoType.equals(IGizmo.Type.LEFT_FLIPPER))
+                            pointX -= 1;
+
+                        if (!canAddFlipper(pointX, pointY)) {
+                            System.out.println("Another flipper occupies the adjacent square! Gizmo cannot be added!");
+                            return;
+                        }
+                    }
+
+                    IGizmo gizmo = gizmoFactory.createGizmo(gizmoType, pointX, pointY);
+                    System.out.println(gizmo);
+                    gameModel.addGizmo(gizmo);
+
+                } else {
+                    System.out.println(mouseEvent.getEventType());
+                }
+            } else {
+                System.out.println("A " + gameModel.getGizmo(pointX, pointY).getType().toString() + " is already occupying this square. Gizmo cannot be placed here!");
             }
-        } else {
-            System.out.println("A " + gameModel.getGizmo(pointX, pointY).getType().toString() + " is already occupying this square. Gizmo cannot be placed here!");
+        } catch (NullPointerException ex) {
+            System.out.print("CHECK OUT! --> ");
+            ex.printStackTrace();
         }
+    }
+
+    private boolean canAddFlipper (double pointX, double pointY) {
+        for (int x = 0; x < 2; x++) {
+            for (int y = 0; y < 2; y++) {
+                if (gameModel.getGizmo(pointX + x, pointY + y) != null)
+                    return false;
+            }
+        }
+        return true;
     }
 }
