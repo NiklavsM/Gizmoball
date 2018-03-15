04e9f2efd97c626a21555ad2a44f6115b4c1d043
Lyubomir Ivanov <kwb15150@uni.strath.ac.uk>
2018-03-15 06:21:55 +0000

    tests added

--- tests/strath/cs308/gizmoball/model/gizmo/GizmoTest.java
+++ tests/strath/cs308/gizmoball/model/gizmo/GizmoTest.java
@@ -3,9 +3,12 @@ package strath.cs308.gizmoball.model.gizmo;
 import mit.physics.Vect;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import strath.cs308.gizmoball.model.Dot;
 import strath.cs308.gizmoball.model.GameModel;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GizmoTest {
     private GameModel model;
@@ -49,5 +52,34 @@ public class GizmoTest {
     @Test
     void testGizmoRotation() {
         Triangle gizmo = new Triangle(18, 18, "triangle");
+        List<Dot> dots = gizmo.getDots();
+        for (int i = 0; i <= 3; i++)
+            gizmo.rotate();
+
+        for (int d = 0; d < dots.size(); d++) {
+            assertEquals(dots.get(d).getX(), gizmo.getDots().get(d).getX(), "After 4 rotations, the triangle's end points (X coordinate) should be exactly the same!");
+            assertEquals(gizmo.getDots().get(d).getY(), dots.get(d).getY(), "After 4 rotations, the triangle's end points (X coordinate) should be exactly the same!");
+        }
+    }
+
+    @Test
+    void testNoIdConstructors() {
+        Triangle triangle = new Triangle(11, 5);
+        CircleGizmo circle = new CircleGizmo(7, 16);
+        Square square = new Square(4, 4);
+
+        assertNotNull(triangle.getId(), "Triangle with null id!");
+        assertNotNull(circle.getId(), "Circle with null id!");
+        assertNotNull(square.getId(), "Square with null id!");
+    }
+
+    @Test
+    void testUniqueIds() {
+        Triangle triangle1 = new Triangle(8, 2);
+        Triangle triangle2 = new Triangle(3, 13);
+        Square square = new Square(17, 5);
+
+        assertTrue(!triangle1.getId().equals(triangle2.getId()) && !triangle2.getId().equals(triangle1.getId()), "Triangles with same ids!");
+        assertFalse(square.getId().equals(triangle1) || square.equals(triangle2.getId()), "Circle with null id!");
     }
 }
