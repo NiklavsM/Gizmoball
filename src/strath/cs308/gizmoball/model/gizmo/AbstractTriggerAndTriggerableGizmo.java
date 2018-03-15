02c60b36adfcefa0b17cf864b4c64ae45b9da404
Ioan Luca <ioan.luca.2016@uni.strath.ac.uk>
2018-03-15 17:32:37 +0000

    Fix code

--- src/strath/cs308/gizmoball/model/gizmo/AbstractTriggerAndTriggerableGizmo.java
+++ src/strath/cs308/gizmoball/model/gizmo/AbstractTriggerAndTriggerableGizmo.java
@@ -3,12 +3,34 @@ package strath.cs308.gizmoball.model.gizmo;
 import strath.cs308.gizmoball.model.triggeringsystem.IAction;
 import strath.cs308.gizmoball.model.triggeringsystem.ITriggerable;
 
+import java.util.List;
+
 public abstract class AbstractTriggerAndTriggerableGizmo extends AbstractTriggerGizmo implements ITriggerable {
 
     private IAction action;
 
     public AbstractTriggerAndTriggerableGizmo(double x1, double y1, double x2, double y2, String id) {
         super(x1, y1, x2, y2, id);
+    }
+
+    @Override
+    public void trigger() {
+        super.trigger();
+    }
+
+    @Override
+    public IAction getCurrentAction() {
+        return null;
+    }
+
+    @Override
+    public List<IAction> getAvailableActions() {
+        return null;
+    }
+
+    @Override
+    public List<IAction> addAvailableAction() {
+        return null;
     }
 
     @Override
