642452f5e29d5e133dd3bdfb6ea5105cdd5242a5
Martin Kollie <martinkollie@gmail.com>
2018-03-20 19:18:51 +0000

    Add icon

--- src/strath/cs308/gizmoball/GizmoBall.java
+++ src/strath/cs308/gizmoball/GizmoBall.java
@@ -3,19 +3,25 @@ package strath.cs308.gizmoball;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import strath.cs308.gizmoball.controller.GameLoader;
 import strath.cs308.gizmoball.controller.InGameKeyEventHandler;
 import strath.cs308.gizmoball.model.GameModel;
 import strath.cs308.gizmoball.model.IGameModel;
 import strath.cs308.gizmoball.model.UndoRedo;
+import strath.cs308.gizmoball.model.gizmo.Gizmo;
 import strath.cs308.gizmoball.model.gizmo.Triangle;
 import strath.cs308.gizmoball.model.triggeringsystem.ITrigger;
 import strath.cs308.gizmoball.utils.Logger;
 import strath.cs308.gizmoball.view.EditorView;
 import strath.cs308.gizmoball.view.PlayView;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.Locale;
 
 public class GizmoBall extends Application {
@@ -43,7 +49,6 @@ public class GizmoBall extends Application {
                 e.printStackTrace();
             }
 
-
             UndoRedo.INSTANCE.saveState(gameModel);
 
             //Doesn't work in xml
@@ -55,12 +60,22 @@ public class GizmoBall extends Application {
             primaryStage.setScene(new PlayView(gameModel));
             primaryStage.show();
             stage = primaryStage;
+            setIcon();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private static void setIcon() {
+        Image image = new Image("/images/icon.png");
+        stage.getIcons().add(image);
+//        Application.getApplication().setDockIconImage(new ImageIcon("Football.png").getImage());
+
+    }
+
     public static void switchView(Scene view) {
+        setIcon();
         stage.setScene(view);
     }
 
