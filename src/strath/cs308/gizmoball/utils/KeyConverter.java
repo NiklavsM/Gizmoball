ec3ee070ab4fa4c1420a8a25bbf84b17cebcbb3d
Martin Kollie <martinkollie@gmail.com>
2018-03-20 14:43:49 +0000

    Fix grid alignment

---
+++ src/strath/cs308/gizmoball/utils/KeyConverter.java
@@ -0,0 +1,20 @@
+package strath.cs308.gizmoball.utils;
+
+import javafx.scene.input.KeyEvent;
+
+import static javafx.scene.input.KeyEvent.KEY_PRESSED;
+import static javafx.scene.input.KeyEvent.KEY_RELEASED;
+
+public class KeyConverter {
+
+    public static String prettify(KeyEvent event) {
+        String type = "";
+        if (event.getEventType().equals(KEY_PRESSED)) {
+            type = "down";
+        } else if (event.getEventType().equals(KEY_RELEASED)) {
+            type = "up";
+        }
+        return "key " + event.getCode().impl_getCode() + ".0 " + type;
+    }
+
+}
